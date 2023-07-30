5b75ca760e6d28b5ee555db99b14f84f33ac536f
Christophe Hurpeau <christophe@hurpeau.com>
2023-07-30 23:29:17 +0200

    build: dist

--- packages/pob-test-typescript-package-in-monorepo/dist/index-node18.mjs
+++ packages/pob-test-typescript-package-in-monorepo/dist/index-node18.mjs
@@ -1,5 +1,11 @@
 import name from 'pob-test-package-in-monorepo';
 
+/* Testing simple feature6 */
+/* Testing simple feature7 */
+/* Testing simple feature8 */
+/* Testing simple feature9 */
+/* Testing simple feature11 */
+/* Testing simple feature12 */
 const index = `Hello ${name}!`;
 
 export { index as default };
