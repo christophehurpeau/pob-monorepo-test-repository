39d69ea737579a033358aed387537776f641d343
Christophe Hurpeau <christophe@hurpeau.com>
2023-07-30 22:53:23 +0200

    feat: test feat9

--- packages/pob-test-typescript-package-in-monorepo/src/index.ts
+++ packages/pob-test-typescript-package-in-monorepo/src/index.ts
@@ -3,4 +3,5 @@ import name from 'pob-test-package-in-monorepo';
 /* Testing simple feature6 */
 /* Testing simple feature7 */
 /* Testing simple feature8 */
+/* Testing simple feature9 */
 export default `Hello ${name}!`;
